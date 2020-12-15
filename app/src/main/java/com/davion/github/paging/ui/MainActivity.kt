f7ffe65edbbc7ab76b8995c98ab01c08f93dcb87
dragonddavion <lequanghuy2410@gmail.com>
2020-12-15 10:13:06 +0700

    Setup the advaned bottom navigation

--- app/src/main/java/com/davion/github/paging/ui/MainActivity.kt
+++ app/src/main/java/com/davion/github/paging/ui/MainActivity.kt
@@ -3,37 +3,37 @@ package com.davion.github.paging.ui
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.davion.github.paging.R
+import com.davion.github.paging.util.setupWithNavControllerCustom
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        init()
+        setupToolbar()
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        finish()
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        return currentNavController?.value?.navigateUp() ?: false
     }
 
     private fun setupToolbar() {
@@ -45,7 +45,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun init() {
-        setupToolbar()
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.users, R.navigation.repos)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavControllerCustom(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, { navController ->
+            val appBarConfiguration = AppBarConfiguration(navController.graph)
+            toolbar_basic.setupWithNavController(navController, appBarConfiguration)
+        })
+        currentNavController = controller
     }
 }
